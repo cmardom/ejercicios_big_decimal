57a796d370791a745b6ed62764b9d79bf6dab693
cmardom599@g.educaand.es <cmardom599@g.educaand.es>
2023-09-25 18:54:55 +0200

    Ej Factorial e InteresCompuesto

--- src/main/java/org/iesvdm/Factorial.java
+++ src/main/java/org/iesvdm/Factorial.java
@@ -32,7 +32,23 @@ public class Factorial {
 
     public BigInteger calcula() {
         //TODO
-        return null;
+//        /* factorial de 5 = 5 X 4 X 3 X 2 X 1 = 120;*/
+        BigInteger resultado;
+        if (n == 0)
+        {
+            //Si n es 0, el facotrial es 1
+            return BigInteger.ONE;
+        } else if (n < 0)
+        {
+            //los negativos no tienen factorial
+            throw new RuntimeException("no se puede calcular el factorial de un numero negativo");
+        } else {
+            resultado = BigInteger.ONE;
+            for (int i = 1; i <= n ; i++) {
+                resultado = resultado.multiply(BigInteger.valueOf(i));
+            }
+        }
+        return resultado;
     }
 
 }
--- src/main/java/org/iesvdm/InteresCompuesto.java
+++ src/main/java/org/iesvdm/InteresCompuesto.java
@@ -47,10 +47,19 @@ public class InteresCompuesto {
         this.c = c;
     }
 
-    public BigDecimal calculaMontoFinal() {
-        //TODO
-        return null;
+    public BigDecimal calculaMontoFinal(int t) {
+        //Calcular el monto final después de t años con interés compuesto
+        // A = P(1+r/n)^(nt)
+        // a. monto total acumulado
+        // p. capital inicial
+        // r. tasa de interes anual en forma decimal
+        // n. numero de veces que se capitaliza el interes por año
+        // t. numero de años
+        BigDecimal base = r.divide(BigDecimal.valueOf(n), 10, BigDecimal.ROUND_HALF_UP).add(BigDecimal.ONE);
+        int exponente = n*t;
+        return p.multiply(base.pow(exponente));
     }
+
 
 
 
