7b54604736f5a52123e22cb630babdae684d7bae
Carmen <cmardom599@g.educaand.es>
2023-09-26 12:44:34 +0200

    factorial arreglado

--- src/main/java/org/iesvdm/Factorial.java
+++ src/main/java/org/iesvdm/Factorial.java
@@ -33,22 +33,12 @@ public class Factorial {
     public BigInteger calcula() {
         //TODO
 //        /* factorial de 5 = 5 X 4 X 3 X 2 X 1 = 120;*/
-        BigInteger resultado;
-        if (n == 0)
-        {
-            //Si n es 0, el facotrial es 1
-            return BigInteger.ONE;
-        } else if (n < 0)
-        {
-            //los negativos no tienen factorial
-            throw new RuntimeException("no se puede calcular el factorial de un numero negativo");
-        } else {
-            resultado = BigInteger.ONE;
-            for (int i = 1; i <= n ; i++) {
-                resultado = resultado.multiply(BigInteger.valueOf(i));
-            }
+        this.factorial = BigInteger.ONE;
+        for (int i = 1; i <= n ; i++) {
+            this.factorial = this.factorial.multiply(BigInteger.valueOf(i));
         }
-        return resultado;
+
+        return this.factorial;
     }
 
 }
